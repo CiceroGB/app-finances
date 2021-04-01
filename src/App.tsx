d85c546ea5822dba9010e2c87e0df609e8d0c538
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-04-01 11:02:18 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Header } from "./components/Header"
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>hellho</h1>
+    <>
+      <Header />
+   
       <GlobalStyle />
-    </div>
+    </>
   );
 }
+
 
 
