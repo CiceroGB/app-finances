7ba5002ba0e2c542144c82dc392749108bea1f1e
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-04-09 12:46:03 -0300

    config modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header"
+import { useState } from "react";
+
+import Modal from "react-modal";
 
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('root');
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen} />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModalOpen}
+      >
+        <h2>Cadastar transação</h2>
+      </Modal>
+
+
       <Dashboard />
       <GlobalStyle />
     </>
