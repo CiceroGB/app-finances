7d2052c6878f66e3e84b544e379e98ef2dd14323
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-04-14 16:22:36 -0300

    Modal config

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header"
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import Modal from 'react-modal';
 
 import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
 
 
 
